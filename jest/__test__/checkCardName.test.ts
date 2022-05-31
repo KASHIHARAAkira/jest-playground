d379644b1b322ee1d206fec93d25c97c5171af0c
Akira Kashihara <akira.kashihara@hotmail.com>
2022-05-31 18:26:13 +0900

    Initial commit

---
+++ jest/__test__/checkCardName.test.ts
@@ -0,0 +1,47 @@
+import OperateMariadb from "../lib/operate_mariadb";
+
+describe("Interface testing.", () => {
+  it("Expected return value is true.", async () => {
+    const operateMariadb = new OperateMariadb();
+    await operateMariadb.getConnection();
+    const result = await operateMariadb.checkCardName("king");
+    expect(result).toBeTruthy();
+    operateMariadb.poolEnd();
+  });
+
+  it("Expected return value is false.", async () => {
+    const operateMariadb = new OperateMariadb();
+    await operateMariadb.getConnection();
+    const result = await operateMariadb.checkCardName("jack");
+    expect(result).toBeFalsy();
+    operateMariadb.poolEnd();
+  });
+});
+
+describe("Error handling testing.", () => {
+  it("con.query throws error", async () => {
+    const operateMariadb = new OperateMariadb();
+    await operateMariadb.getConnection();
+    if (operateMariadb.connection) {
+      jest
+        .spyOn(operateMariadb.connection, "query")
+        .mockImplementation(() => Promise.reject(new Error("Query Error.")));
+
+      expect(operateMariadb.checkCardName("king")).rejects.toThrow(
+        "Query Error."
+      );
+
+      operateMariadb.poolEnd();
+    }
+  });
+
+  it("this.connection is undefined.", () => {
+    const operateMariadb = new OperateMariadb();
+
+    expect(operateMariadb.checkCardName("king")).rejects.toThrow(
+      "The database connection does not exist."
+    );
+
+    operateMariadb.poolEnd();
+  });
+});
